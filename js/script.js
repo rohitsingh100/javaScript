efb07767385dc37a1756c212385d8029fc939aa0
Rohit Singh <114247566+rohitsingh100@users.noreply.github.com>
2023-09-17 19:47:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ book.forEach(myfu);
 function myfu(value){
     console.log(value);
 }
+
+// Table
+//function Table(num){
+    // let num=2;
+    //document.write("This is a Table...<br>");
+//for(i=1;i<=10;i++){
+                 // 2      X      1      =       2
+    //document.write(num + " X " +  i  + " = " + num*i +"<br>");
+    //}
+//}
+//Table(2);
+//Table(3);
